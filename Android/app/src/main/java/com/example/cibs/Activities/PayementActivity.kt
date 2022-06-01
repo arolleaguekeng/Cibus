33e8f49b280108b4b13863b83997b0fa4a101da2
nokizemaxime <93329117+nokizemaxime@users.noreply.github.com>
2022-06-02 00:14:45 +0100

    Activite de la langue et bottomsheet dialog des parametre et l'activite des payement

---
+++ Android/app/src/main/java/com/example/cibs/Activities/PayementActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cibs.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cibs.R
+
+class PayementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payement)
+    }
+}
